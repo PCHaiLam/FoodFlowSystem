0dee3b29ef14a1b8ad303520930ff2323194b260
Phan Châu Hải Lâm <phanchauhailam15803@gmail.com>
2025-03-17 08:56:12 +0700

    CRUD category

---
+++ Services/Category/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using FoodFlowSystem.DTOs.Requests.Category;
+using FoodFlowSystem.DTOs.Responses;
+
+namespace FoodFlowSystem.Services.Category
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryResponse>> GetCategoriesAsync();
+        Task<CategoryResponse> GetCategoryByIdAsync(int id);
+        Task<CategoryResponse> CreateCategoryAsync(CreateCategoryRequest request);
+        Task<CategoryResponse> UpdateCategoryAsync(UpdateCategoryRequest request);
+        Task DeleteCategoryAsync(int id);
+    }
+}
---
+++ Validators/Category/CreateCategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using FoodFlowSystem.DTOs.Requests.Category;
+
+namespace FoodFlowSystem.Validators.Category
+{
+    public class CreateCategoryValidator : AbstractValidator<CreateCategoryRequest>
+    {
+        public CreateCategoryValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Name).MaximumLength(100).WithMessage("Name must not exceed 100 characters");
+            RuleFor(x => x.Description).MaximumLength(200).WithMessage("Description must not exceed 200 characters");
+        }
+    }
+}
